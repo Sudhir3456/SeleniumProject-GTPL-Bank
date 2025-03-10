51766ac253e5f838280e5d811c75ad7bbb9a3782
sudhir <sy6841077@gmail.com>
2025-03-10 22:45:23 +0530

    hh

---
+++ src/test/java/com/inetBanking/testCases/BaseClass.java
@@ -0,0 +1,75 @@
+package com.inetBanking.testCases;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+//import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+
+import com.inetBanking.utilities.ReadConfig;
+
+public class BaseClass {
+
+	ReadConfig readconfig = new ReadConfig();
+
+	public String baseURL = readconfig.getApplicationURL();
+	public String username = readconfig.getUsername();
+	public String password = readconfig.getPassword();
+	public static WebDriver driver;
+
+	@BeforeClass
+	public void toLaunchBrowser(@Optional("chrome") String browser) throws IOException {
+
+		//String url = username;
+
+		Reporter.log("Launching " + browser + " successfully", true);
+		if (browser.equalsIgnoreCase("chrome"))
+			driver = new ChromeDriver();
+		else if (browser.equalsIgnoreCase("firefox"))
+			driver = new FirefoxDriver();
+		else if (browser.equalsIgnoreCase("edge"))
+			driver = new EdgeDriver();
+		else
+			Reporter.log("Invalid Browser");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get(baseURL);
+
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
+	public void captureScreen(WebDriver driver, String tname) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File target = new File(System.getProperty("user.dir") + "/Screenshots/" + tname + ".png");
+		FileUtils.copyFile(source, target);
+		System.out.println("Screenshot taken");
+	}
+
+	public String randomestring() {
+		String generatedstring = RandomStringUtils.randomAlphabetic(8);
+		return (generatedstring);
+	}
+
+	public static String randomeNum() {
+		String generatedString2 = RandomStringUtils.randomNumeric(4);
+		return (generatedString2);
+	}
+
+}
